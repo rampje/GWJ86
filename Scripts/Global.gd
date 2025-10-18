3c82075a2f7ca2b7cc5991d05c9be1f9d1599249
Warner Rose <wajorose@gmail.com>
2025-10-17 22:13:35 -0400

    built ~3 rooms of endgame, time mask spot

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -36,11 +36,11 @@ func _ready() -> void:
 	#enable_all()
 	#has_jump = true
 
-#func _input(event):
-	#if event.is_action_pressed("reload"):
-		#reset_game()
-		#enable_all()
-		#get_tree().reload_current_scene()
+func _input(event):
+	if event.is_action_pressed("reload"):
+		reset_game()
+		enable_all()
+		get_tree().reload_current_scene()
 
 
 func reset_game() -> void:
